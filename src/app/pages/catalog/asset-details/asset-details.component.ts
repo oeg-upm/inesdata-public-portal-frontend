28adc545e6f0030d7a07e8dffef6a360b443e094
ppel <ppel@gmv.com>
2024-08-01 08:59:21 +0200

    #439394 - Bugfix navigation in the catalog section

--- src/app/pages/catalog/asset-details/asset-details.component.ts
+++ src/app/pages/catalog/asset-details/asset-details.component.ts
@@ -3,6 +3,8 @@ import { Router } from '@angular/router';
 import { DataOffer } from '../../../shared/models/data-offer';
 import { DomSanitizer } from '@angular/platform-browser';
 import { ASSET_TYPES } from 'src/app/shared/utils/app.constants';
+import { FormGroup } from '@angular/forms';
+import { Vocabulary } from 'src/app/shared/models/vocabulary';
 
 
 @Component({
@@ -15,11 +17,19 @@ export class AssetDetailsComponent implements OnInit {
 	dataset: DataOffer;
 	currentPage: number;
 	pageSize: number;
+	formValues: FormGroup[] = [];
+	searchText: String = undefined;
+	filterExpression: any[] = [];
+	vocabulariesDefinition: Vocabulary[] = [];
 
   constructor(private router: Router, private sanitizer: DomSanitizer) {
 		this.dataset = this.router.getCurrentNavigation().extras.state.dataset;
 		this.currentPage = this.router.getCurrentNavigation().extras.state.currentPage;
 		this.pageSize = this.router.getCurrentNavigation().extras.state.pageSize;
+		this.formValues = this.router.getCurrentNavigation().extras.state.formValues;
+		this.searchText = this.router.getCurrentNavigation().extras.state.searchText;
+		this.filterExpression = this.router.getCurrentNavigation().extras.state.filterExpression;
+		this.vocabulariesDefinition = this.router.getCurrentNavigation().extras.state.vocabulariesDefinition;
 		this.dataset.properties.description = this.sanitizer.bypassSecurityTrustHtml(this.dataset.properties.description);
   }
 
@@ -30,7 +40,11 @@ export class AssetDetailsComponent implements OnInit {
 		this.router.navigate(['/catalog'], {
       state: {
 				currentPage: this.currentPage,
-				pageSize: this.pageSize
+				pageSize: this.pageSize,
+				formValues: this.formValues,
+				searchText: this.searchText,
+				filterExpression: this.filterExpression,
+				vocabulariesDefinition: this.vocabulariesDefinition
 			}
     });
 	}
