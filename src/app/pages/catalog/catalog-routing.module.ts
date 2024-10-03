e8a1e3fa5daf874bf4c85b50a0921b72e60aa621
ppel <ppel@gmv.com>
2024-10-03 15:27:15 +0200

    #444866 - Now the dataset details can be accesed by URL

--- src/app/pages/catalog/catalog-routing.module.ts
+++ src/app/pages/catalog/catalog-routing.module.ts
@@ -3,12 +3,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AssetDetailsComponent } from './asset-details/asset-details.component';
 import { CatalogBrowserComponent } from './catalog-browser/catalog-browser.component';
-import { BrowserModule } from '@angular/platform-browser';
-import { ReactiveFormsModule } from '@angular/forms';
 
 const routes: Routes = [
   { path: '', component: CatalogBrowserComponent },
-	{ path: ':id', component: AssetDetailsComponent }
+	{ path: ':participantId/:datasetId', component: AssetDetailsComponent }
 ];
 
 @NgModule({
